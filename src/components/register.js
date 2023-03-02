13cee7eb71e6ba731ad058283c6165233824e118
animesh-kumar-chaturvedi <52943258+Chaturvedi-Animesh@users.noreply.github.com>
2023-03-02 14:05:01 +0530

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -1,106 +1,241 @@
-import { Button, Card, CardContent, CardHeader, TextField } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 
-function Register(){
+function Register() {
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    age: "",
+    gender: "",
+    address: "",
+  });
 
-    const [userDetails,setUserDetails]= useState({
-        name : "",
-        email : "",
-        password : "",
-        age: "",
-        gender: ""
-    })
+  const [error, setError] = useState({
+    nameError: "",
+    emailError : "",
+    passwordError: "",
+    ageError: ""
+  });
 
-    function handleOnChange(event){
-        setUserDetails({
-            ...userDetails,
-            [event.target.id] : event.target.value
-        })
+  const [isDisabled,setIsDisabled] = useState(true)
+ 
+
+  function handleOnChange(event) {
+    setUserDetails({
+      ...userDetails,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleOnBlur(event) {
+    if(event.target.name === "name"){
+        if(userDetails.name === "") {
+            setError({
+                ...error,
+                nameError : "Name Cannot be Empty"
+            })
+        }
+        else{
+            setError({
+                ...error,
+                nameError : ""
+            })
+        }
     }
 
-    function handleOnSubmit(){
-        console.log(userDetails)
+    if(event.target.name === "email"){
+        if(userDetails.email === "") {
+            setError({
+                ...error,
+                emailError : "Email Cannot be Empty"
+            })
+        }
+        else if(!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(userDetails.email))){
+            setError({
+                ...error,
+                emailError : "Wrong Email Format"
+            })
+        }
+        else{
+            setError({
+                ...error,
+                emailError : ""
+            })
+        }
+    }
+    if(event.target.name === "password"){
+        if(userDetails.password === "") {
+            setError({
+                ...error,
+                passwordError : "Password Cannot be Empty"
+            })
+        }
+        else{
+            setError({
+                ...error,
+                passwordError : ""
+            })
+        }
     }
 
+    if(event.target.name === "age"){
+        if(userDetails.age === "") {
+            setError({
+                ...error,
+                ageError : "Age Cannot be Empty"
+            })
+        }
+        else if( parseInt(userDetails.age) <= 0){
+            setError({
+                ...error,
+                ageError : "Age Cannot be Less than 1"
+            })
+        }
+        else{
+            setError({
+                ...error,
+                ageError : ""
+            })
+        }
+    }
 
+    if(Object.values(error).every(x => x==="")){
+        setIsDisabled(false)
+    }
+    if(userDetails.name === "" || userDetails.password === "" || userDetails.age === "" || userDetails.email === ""){
+        setIsDisabled(true)
+    }
 
-    return(
-        <Card sx={{
-            width: 'fit-content',
-            m: 'auto',
-            boxShadow: 12
-        }}>
-            <CardHeader title="Register"/>
-            <CardContent sx={{
-                m: 1/2,
-                display: 'flex',
-                flexDirection : 'column',
-                justifyContent : 'space-around',
-                gap: '10px'
-            }}>
-                {/* Name TextField */}
-                <TextField
-                required
-                id="name"
-                label="Name"
-                value={userDetails.name}
-                onChange={handleOnChange}
-                // helperText={userDetails.emailError ? "Invalid Email" : ""}
-                // error = {userDetails.emailError}
-                // onBlur= {handleOnChange}
-                 />
+  }
 
-                 {/* Email TextField */}
-                 <TextField 
-                 required
-                 id="email"
-                 label="Email"
-                 value={userDetails.email}
-                 onChange={handleOnChange}
-                 />
+  function handleOnSubmit() {
+    console.log(userDetails);
+    console.log(Object.values(error).every(x => x===""))
+  }
 
-                 {/* PasswordTextfiled */}
+//   const isDisabled = () =>{
+//     console.log(Object.values(error).every(x => x===""))
+//     if(Object.values(error).every(x => x==="")) return false
 
-                 <TextField 
-                 required
-                 id="password"
-                 label="Password"
-                 value={userDetails.password}
-                 onChange={handleOnChange}
-                 />
+//     return true
+//   }
 
-                 {/* Age TextField                  */}
-                 <TextField 
-                 required
-                 type="number"
-                 id="age"
-                 label="Age"
-                 value={userDetails.age}
-                 onChange={handleOnChange}
-                 />
+  return (
+    <Card
+      sx={{
+        width: "fit-content",
+        m: "auto",
+        boxShadow: 12,
+      }}
+    >
+      <CardHeader title="Register" />
+      <CardContent
+        sx={{
+          m: 1 / 2,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          gap: "10px",
+        }}
+      >
+        {/* Name TextField */}
+        <TextField
+          required
+          name="name"
+          label="Name"
+          value={userDetails.name}
+          onChange={handleOnChange}
+          helperText={error.nameError}
+          error={error.nameError === ""?false:true}
+          onBlur= {handleOnBlur}
+        />
 
-                 {/* Gender TextField */}
-                 
+        {/* Email TextField */}
+        <TextField
+          required
+          name="email"
+          label="Email"
+          value={userDetails.email}
+          onChange={handleOnChange}
+          helperText={error.emailError}
+          error={error.emailError === ""?false:true}
+          onBlur= {handleOnBlur}
+        />
 
-                 {/* Register Button */}
-                <Button 
-                variant="contained" 
-                onClick={handleOnSubmit} 
-                // disabled={userDetails.emailError || userDetails.passwordError}
-                >Register</Button>
-                {/* Register Button */}
-                <Button 
-                variant="text"
-                href="/login"
-                >Already Registered? Login</Button>
+        {/* PasswordTextfiled */}
 
+        <TextField
+          required
+          name="password"
+          label="Password"
+          type={"password"}
+          value={userDetails.password}
+          onChange={handleOnChange}
+          helperText={error.passwordError}
+          error={error.passwordError === ""?false:true}
+          onBlur= {handleOnBlur}
+        />
 
+        {/* Age TextField                  */}
+        <TextField
+          required
+          type="number"
+          name="age"
+          label="Age"
+          value={userDetails.age}
+          onChange={handleOnChange}
+          helperText={error.ageError}
+          error={error.ageError === ""?false:true}
+          onBlur= {handleOnBlur}
+        />
 
+        {/* address TextField */}
 
+        <TextField
+          name="address"
+          label="Address"
+          value={userDetails.address}
+          onChange={handleOnChange}
+        />
 
-            </CardContent>
-        </Card>
-    )
+        {/* Gender TextField */}
+        <TextField
+          value={userDetails.gender}
+          onChange={handleOnChange}
+          select // tell TextField to render select
+          label="Gender"
+          name="gender"
+        >
+          <MenuItem value={"Male"}>Male</MenuItem>
+          <MenuItem value={"Female"}>Female</MenuItem>
+          <MenuItem selected value="Prefer Not To Say">
+            Prefer Not To Say
+          </MenuItem>
+        </TextField>
+
+        {/* Register Button */}
+        <Button
+          variant="contained"
+          onClick={handleOnSubmit}
+          disabled={isDisabled}
+        >
+          Register
+        </Button>
+        {/* Register Button */}
+        <Button variant="text" href="/login">
+          Already Registered? Login
+        </Button>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Register
+export default Register;
