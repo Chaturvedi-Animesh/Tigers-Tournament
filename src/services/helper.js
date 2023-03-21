7e11895b5a85507c90870190e8f37836b56f07bc
animesh-kumar-chaturvedi <52943258+Chaturvedi-Animesh@users.noreply.github.com>
2023-03-21 12:20:40 +0530

    Update helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-const BASE_URL = "https://13.233.214.100:8082/kridabuzz"
+const BASE_URL = "http://13.233.214.100:8082/kridabuzz"
 
 export const myAxios = axios.create({baseURL : BASE_URL})
